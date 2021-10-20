fc0a7d83493608464c187ec9cfab682b3f4bde40
lanyo estate <ashulemuel@gmail.com>
2021-10-20 21:56:05 +0100

    changed hero img

--- components/Homehero.jsx
+++ components/Homehero.jsx
@@ -145,7 +145,7 @@ export default function Example() {
       <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
         <img
           className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
-          src="./images/hero.jpg"
+          src="./images/hero.svg"
           alt=""
         />
       </div>
